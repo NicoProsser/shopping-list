fd82a1a1192a16268bde903423a3a718dc2015e3
NicoProsser <prossernico@gmail.com>
2025-05-25 20:25:59 +0200

    Created functionality to add new items + validators

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Grocery List')),
+      appBar: AppBar(
+        title: const Text('Grocery List'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder: (ctx, index) => ListTile(
+          // Name des Lebensmittels
           title: Text(groceryItems[index].name),
+          // Farbliche Markierung der Kategorie
           leading: Container(
             width: 24,
             height: 24,
             color: groceryItems[index].category.color,
           ),
+          // Anzeige der Menge
           trailing: Text(groceryItems[index].quantity.toString()),
         ),
       ),
